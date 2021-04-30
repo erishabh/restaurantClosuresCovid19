ff722d677c646372ea9bac363b9f89a5d059b56b
Rishabh Roy <erishabh@yahoo.co.in>
2021-04-30 02:23:33 -0700

    More cleaning

--- SF_open_data_cleaning.py
+++ SF_open_data_cleaning.py
@@ -7,6 +7,28 @@ business_CA = pd.read_csv('SF_open_data_portal_business_raw.csv')
 # Deleteing all rows with address outside CA
 business_CA = business_CA[business_CA['State'] == 'CA']
 
+#################### cleaning city ####################
+
+# Finding all unique city names
+city_set = set(business_CA['City'])
+
+# Converting the set to a dataframe
+city_df = pd.DataFrame(list(city_set))
+
+# Exporting the dataframe as a csv
+city_df.to_csv('SF_open_data_city.csv', index = False, header = True)
+
+print('********************** EXPORT COMPLETE **********************')
+
+# Import manually cleaned list of cities in SF
+city_SF = pd.read_csv('SF_open_data_city_clean.csv')
+
+# Convert city_SF to list
+city_SF_list = city_SF["City"].tolist()
+
+# Extracting rows in SF
+business_CA = business_CA[business_CA['City'].isin(city_SF_list)]
+
 #################### cleaning business address ####################
 
 # Using function to clean address
--- helper_functions.py
+++ helper_functions.py
@@ -8,14 +8,16 @@ def addressCleaner(df, address, city, state, zip_code, new_col):
     addressCleaner(df, address, city, state, zip_code, new_col)
 
     Arguments:
+
     df:         DataFrame with columns that contain the address columns
     address:    Column name in df that contains the street address
-    city:       Column name in df that contains the city
+    city:       Column name in df that contains the city (only used for column deletion)
     state:      Column name in df that contains the state
     zip_code:   Column name in df that contains the zip code
     new_col:    Column name that will contain the consolidated address
 
     Returns:
+    
     df_copy:    A copy of the input df with new_col and all other address columns dropped
     """
 
@@ -24,29 +26,26 @@ def addressCleaner(df, address, city, state, zip_code, new_col):
 
     # Making columns lowercase [Street Address, City]
     df_copy[address] = df_copy[address].str.lower()
-    df_copy[city] = df_copy[city].str.lower()
 
     # Making state uppercase
     df_copy[state] = df_copy[state].str.upper()
 
     # Splitting relevent columns on non-word elements [Street Address, City]
     df_copy['street_add_list'] = df_copy[address].str.split(pat = r'\b\W')
-    df_copy['city_list'] = df_copy[city].str.split(pat = r'\b\W')
 
     # Extracting first 5 digits from zip code
     df_copy['zip_5'] = df_copy[zip_code].astype(str).str[:5]
 
     # Combining lists in clumns with ' '
     df_copy['street_add_list_join'] = df_copy['street_add_list'].str.join(sep = ' ')
-    df_copy['city_list_join'] = df_copy['city_list'].str.join(sep = ' ')
 
     # Joining all street address columns together
-    df_copy[new_col] = (df_copy['street_add_list_join'] + ', ' + df_copy['city_list_join'] 
-        + ', ' + df_copy[state] + ', ' + df_copy['zip_5'])
+    df_copy[new_col] = (df_copy['street_add_list_join'] + ', san francisco, ' + df_copy[state] 
+        + ', ' + df_copy['zip_5'])
 
     # Deleting extraneous columns
-    df_copy.drop(columns = ['street_add_list', 'city_list', 'zip_5', 'street_add_list_join', 
-        'city_list_join', address, city, state, zip_code], inplace = True)
+    df_copy.drop(columns = ['street_add_list', 'zip_5', 'street_add_list_join', 
+        address, city, state, zip_code], inplace = True)
 
     # Returning the dataframe
     return df_copy
